9175e3d09a6155bf155d44bc69c25c8c7d7def63
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-02 10:29:38 -0300

    feat: Add initial Spring Boot application with JWT authentication setup

---
+++ src/main/java/br/com/applogin/backend_applogin/BackendApploginApplication.java
@@ -0,0 +1,13 @@
+package br.com.applogin.backend_applogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApploginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApploginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/applogin/backend_applogin/BackendApploginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.applogin.backend_applogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApploginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
